6d0bd9a7398b66baada39936c1ceccc1e85895fc
Stiven <majestiic-10@hotmail.com>
2023-10-19 11:42:25 -0500

    Añade handlers.go: contiene todos los handlers de la aplicacion web

---
+++ handlers.go
@@ -0,0 +1,18 @@
+package main
+
+import "net/http"
+
+type TemplateData struct {
+}
+
+func (app *Application) HomeHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *Application) LoginHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *Application) AboutHandler(w http.ResponseWriter, r *http.Request) {
+
+}
